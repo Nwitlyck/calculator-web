7770176a25890ff723278939d7bea1efd45e8eac
Nwitlyck <alberto.segura1@ulatina.net>
2022-09-02 06:33:05 -0600

    Final touch for scrip.js v1.1.1

--- javascrip-code/script.js
+++ javascrip-code/script.js
@@ -6,46 +6,67 @@ function banqueo() {
     fistTime = false;
   }
 }
+
+function noOLeaft() {
+  let txt = document.getElementById("textareaCalculator").value;
+  if (txt.valueOf(0) == "0" && txt.valueOf(1) != ".") {
+    txt = txt.slice(1);
+  }
+  document.getElementById("textareaCalculator").value = txt;
+}
 //Numbers from 0 to 9
 document.getElementById("buttonNumOne").onclick = function () {
+  noOLeaft();
   banqueo();
   document.getElementById("textareaCalculator").value += "1";
 };
 document.getElementById("buttonNumTwo").onclick = function () {
+  noOLeaft();
   banqueo();
   document.getElementById("textareaCalculator").value += "2";
 };
 document.getElementById("buttonNumThree").onclick = function () {
+  noOLeaft();
   banqueo();
   document.getElementById("textareaCalculator").value += "3";
 };
 document.getElementById("buttonNumFour").onclick = function () {
+  noOLeaft();
   banqueo();
   document.getElementById("textareaCalculator").value += "4";
 };
 document.getElementById("buttonNumFive").onclick = function () {
   banqueo();
+  noOLeaft();
   document.getElementById("textareaCalculator").value += "5";
 };
 document.getElementById("buttonNumSix").onclick = function () {
+  noOLeaft();
   banqueo();
   document.getElementById("textareaCalculator").value += "6";
 };
 document.getElementById("buttonNumSeven").onclick = function () {
+  noOLeaft();
   banqueo();
   document.getElementById("textareaCalculator").value += "7";
 };
 document.getElementById("buttonNumEight").onclick = function () {
+  noOLeaft();
   banqueo();
   document.getElementById("textareaCalculator").value += "8";
 };
 document.getElementById("buttonNumNine").onclick = function () {
+  noOLeaft();
   banqueo();
   document.getElementById("textareaCalculator").value += "9";
 };
 document.getElementById("buttonNumCero").onclick = function () {
   banqueo();
-  document.getElementById("textareaCalculator").value += "0";
+  let txt = document.getElementById("textareaCalculator").value;
+  if (txt != "0") {
+    txt += "0";
+  }
+  document.getElementById("textareaCalculator").value = txt;
 };
 //Dot
 document.getElementById("buttonCharDot").onclick = function () {
@@ -57,11 +78,19 @@ document.getElementById("buttonCharDot").onclick = function () {
   }
   document.getElementById("textareaCalculator").value = txt;
 };
-//Delete/BackSlash/LeftArrow
+//CE, C, LeftArrow
 document.getElementById("buttonCharLeftArrow").onclick = function () {
   document.getElementById("textareaCalculator").value = document
     .getElementById("textareaCalculator")
     .value.slice(0, -1);
+};
+document.getElementById("buttonStringC").onclick = function () {
+  document.getElementById("textareaCalculator").value = "";
+  cal = ["", "", ""];
+  fistTime = true;
+};
+document.getElementById("buttonStringCE").onclick = function () {
+  document.getElementById("textareaCalculator").value = "";
 };
 //Symbols like ( + , - , * , / )
 document.getElementById("buttonCharPlus").onclick = function () {
@@ -81,10 +110,12 @@ document.getElementById("buttonCharDive").onclick = function () {
   cal[1] = 4;
 };
 //Equal
+
 document.getElementById("buttonCharEqual").onclick = function () {
   let txt = document.getElementById("textareaCalculator").value;
-  if (cal[0] == "") {
-    txt = cal[0];
+  if (txt == "" || txt == "0") {
+    txt = 0;
+  } else if (cal[0] == "") {
   } else {
     cal[2] = txt;
     switch (cal[1]) {
@@ -103,4 +134,6 @@ document.getElementById("buttonCharEqual").onclick = function () {
     }
   }
   document.getElementById("textareaCalculator").value = txt;
+  cal = ["", "", ""];
+  fistTime = true;
 };
